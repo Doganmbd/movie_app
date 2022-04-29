480179e9252f93f8cc476c1dce300375aaa09d94
DMB29 <mbilaldogan90@gmail.com>
2022-04-29 08:23:53 +0300

    displayName active

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword, updateProfile} from "firebase/auth";
 
 
 
@@ -26,9 +26,11 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password,navigate)=> {
+export const createUser = async (email, password,navigate,displayName)=> {
     try{
         let userCredential= await createUserWithEmailAndPassword(auth, email, password)
+        await updateProfile(auth.currentUser, {
+            displayName: displayName , })
         console.log(userCredential);
 
         navigate("/");

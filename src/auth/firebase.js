dca6ce18b1e9faca1db32245e71a3721f5d833da
DMB29 <mbilaldogan90@gmail.com>
2022-04-30 18:45:58 +0300

    ToastNotify create

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword, updateProfile, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut} from "firebase/auth";
+import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword, updateProfile, onAuthStateChanged, GoogleAuthProvider, signInWithPopup,  signOut} from "firebase/auth";
 
 
 

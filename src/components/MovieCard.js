4596b75bcac78a10b527ef59aeef3719df18d2f1
DMB29 <mbilaldogan90@gmail.com>
2022-04-30 15:48:59 +0300

    MovieCard create

---
+++ src/components/MovieCard.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext';
+
+
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+const defaultImage ="https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+const MovieCard = ({title,poster_path,overview,vote_average,id}) => {
+
+    const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="movie">
+        <img src={poster_path ? IMG_API + poster_path : defaultImage } alt="bilal"  />
+        <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+            <h5>{title}</h5>
+            {currentUser && <span className={`tag ${vote_average >= 8 ? "green" : vote_average>= 6 ?"orange":"red"}`} >{vote_average}</span>}
+        </div>
+        <div className="movie-over">
+            <h2>overview</h2>
+            <p>{overview}</p>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import MovieCard from '../components/MovieCard';
+
+
+
+// const API_KEY = "0d146bcd3f23ae8d930010fde1d5a727";
+/* const EXAMPLE_KEY ="https://api.themoviedb.org/3/movie/550?api_key=0d146bcd3f23ae8d930010fde1d5a727" */
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
 
 const Main = () => {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, [])
+
+  const getMovies = async (API) => {
+    try {
+      let awaitApi=await axios.get(API)
+       setMovies(awaitApi.data.results)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+
   return (
-    <div>Main</div>
+    <>
+      <div className="d-flex justify-content-center flex-wrap">
+        {movies.map((movie)=>(
+          <MovieCard key={movie.id} {...movie}/>
+        ))}
+      </div>
+
+    </>
   )
 }
 

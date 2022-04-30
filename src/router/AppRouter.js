91ac580abf322dfa0cbfdee67e68c7c7af155862
DMB29 <mbilaldogan90@gmail.com>
2022-04-30 18:08:49 +0300

    movieDetail v1

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Navbar from '../components/Navbar';
 import Login from '../pages/Login';
 import Main from '../pages/Main';
+import MovieDetails from '../pages/MovieDetails';
 import Register from '../pages/Register';
 
 
@@ -14,6 +15,7 @@ const AppRouter = () => {
         <Route path="/" element={<Main/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/details/:id" element={<MovieDetails/>}/>
       </Routes>
     </Router>
   )
